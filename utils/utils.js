1605930226624bd2dc1358b3ceb05f7710d27e57
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2018-07-17 19:53:38 +0800

    add assertion testing

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,12 @@
 module.exports = {
     add: (a, b) => a + b,
-    square: (a) => a * a 
+    square: (a) => a * a,
+    setName: (user, fullName) => {
+        let names = fullName.split(" ");
+
+        user.firstName = names[0];
+        user.lastName = names[1];
+
+        return user;
+    }
 }
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,13 +1,53 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
-it('Should add two numbers.', function() {
+it('Should add two numbers.', () => {
     let result = utils.add(2, 3);
 
-    if (result !== 5) throw new Error(`Expected 5, but got ${result}`);
+    // if (result !== 5) throw new Error(`Expected 5, but got ${result}`);
+    expect(result)
+        .toBe(5)
+        .toBeA('number');
 });
 
-it('Should square a number', function() {
+it('Should square a number', () => {
     let result = utils.square(5);
 
-    if (result !== 25) throw new Error(`Expected 25, but got ${result}`);
+    // if (result !== 25) throw new Error(`Expected 25, but got ${result}`);
+    expect(result)
+        .toBe(25)
+        .toBeA('number');
 });
+
+it('Should verify first name and last name are set', () => {
+    let result = utils.setName({}, "Eksa Pramindanata");
+    
+    expect(result)
+        .toBeA('object')
+        .toInclude({
+            firstName: "Eksa",
+            lastName: "Pramindanata"
+        });
+});
+
+// it('Should expect some values', () => {
+//     // expect(11).toNotBe(11);
+
+//     // expect({
+//     //     name: "Eksa"
+//     // })
+//     // .toEqual({
+//     //     name: "Eksa"
+//     // });
+
+//     // expect([1, 2, 3, 4, 7]).toInclude(5);
+
+//     expect({
+//         name: "Eksa",
+//         age: 19
+//     })
+//     .toExclude({
+//         age: 23
+//     });
+// });
